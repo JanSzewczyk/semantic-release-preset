aef516560ca518443611907072a975c7099bf8ee
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-02-27 11:00:30 +0100

    feat: add release preset without npm publish

---
+++ without-npm.js
@@ -0,0 +1,16 @@
+/** @type {import('semantic-release').Options} */
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", {
+      changelogTitle: `<!-- markdownlint-disable --><!-- textlint-disable -->
+# 📓 Changelog
+All notable changes to this project will be documented in this file. See
+[Conventional Commits](https://conventionalcommits.org) for commit guidelines.`
+    }],
+    ["@semantic-release/npm", {npmPublish: false}],
+    ["@semantic-release/git", { "message": "chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}", assets: ["CHANGELOG.md", "package.json"] }],
+    "@semantic-release/github"
+  ]
+};
