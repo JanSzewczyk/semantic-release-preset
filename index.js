6f15d4a42f0bc94190f1698041b01e06a21d7d8f
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-02-27 10:47:22 +0100

    feat(release-config): add additional options to configuration

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
+    ["@semantic-release/changelog", {
+      changelogTitle: `<!-- markdownlint-disable --><!-- textlint-disable -->
+# 📓 Changelog
+All notable changes to this project will be documented in this file. See
+[Conventional Commits](https://conventionalcommits.org) for commit guidelines.`
+    }],
     "@semantic-release/npm",
-    ["@semantic-release/git", { assets: ["CHANGELOG.md", "package.json"] }],
+    ["@semantic-release/git", { "message": "chore(release): ${nextRelease.gitTag} [skip ci]\\n\\n${nextRelease.notes}", assets: ["CHANGELOG.md", "package.json"] }],
     "@semantic-release/github"
   ]
 };
